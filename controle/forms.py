63ec2d238876376c2875dbf6cbf6a57a180772a6
leandrosouzaf30 <leandrosouzaf30@gmail.com>
2018-03-31 16:36:40 -0300

    Refatoração de código, correções Models Produto e ItemVendido

--- controle/forms.py
+++ controle/forms.py
@@ -4,7 +4,7 @@ from controle.models import Produto
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
-        fields = ['descricao', 'preco', 'unidades_no_estoque', 'unidades_pedidas', 'categoria', 'fornecedor']
+        fields = ['descricao', 'preco', 'unidades_compradas', 'categoria', 'fornecedor']
 
     def __init__(self, *args, **kwargs):
         super(ProdutoForm, self).__init__(*args, **kwargs)
@@ -12,7 +12,7 @@ class ProdutoForm(forms.ModelForm):
         self.fields['descricao'].widget.attrs['class'] = 'form-control'
         self.fields['preco'].widget.attrs['class'] = 'form-control'
         self.fields['categoria'].widget.attrs['class'] = 'form-control'
-        self.fields['unidades_pedidas'].widget.attrs['class'] = 'form-control'
-        self.fields['unidades_no_estoque'].widget.attrs['class'] = 'form-control'
+        self.fields['unidades_compradas'].widget.attrs['class'] = 'form-control'
+        #self.fields['unidades_no_estoque'].widget.attrs['class'] = 'form-control'
         self.fields['fornecedor'].widget.attrs['class'] = 'form-control'
     
--- controle/models.py
+++ controle/models.py
@@ -22,12 +22,12 @@ class Fornecedor(models.Model):
 
 class Produto(models.Model):
     descricao = models.CharField(max_length=255)
-    preco = models.FloatField()
-    unidades_no_estoque = models.IntegerField()
-    unidades_pedidas = models.IntegerField()
-    imagem = models.ImageField(
-        upload_to='controle/images', null=True, blank=True
-    )
+    preco = models.FloatField(default=0.0)
+    unidades_compradas = models.IntegerField()
+    # unidades_no_estoque = models.IntegerField()
+    # imagem = models.ImageField(
+    #     upload_to='controle/images', null=True, blank=True
+    # )
     categoria = models.ForeignKey(
         "Categoria",
         on_delete=models.CASCADE,
@@ -37,15 +37,15 @@ class Produto(models.Model):
         on_delete=models.CASCADE
     )
 
-    def qtd_estoque(self):
-        return self.unidades_no_estoque - self.unidades_pedidas
+    # def qtd_estoque(self):
+    #     return self.unidades_no_estoque - self.unidades_pedidas
 
     def __str__(self):
         return self.descricao
 
 
 class ItemVendido(models.Model):
-    preco_unitario = models.FloatField(verbose_name='Valor da Venda')
+    preco_unitario = models.FloatField(default= 0.0, editable=False)
     quantidade = models.IntegerField()
     desconto = models.FloatField(default=0.0)
     data_venda = models.DateField()
@@ -58,8 +58,17 @@ class ItemVendido(models.Model):
         on_delete=models.CASCADE,
     )
 
+    # Custom save method in model [ItemVendido]
+    def get_preco_produto(self):
+        self.preco_unitario = self.produto.preco
+    
+    def save(self, *args, **kwargs):
+        self.get_preco_produto()        
+        super(ItemVendido, self).save(*args, **kwargs)
+
+    #Valor do produto se for dado desconto
     def valor_final_prod(self):
-        return (self.preco_unitario - self.desconto)
+        return (self.produto.preco - self.desconto) * self.quantidade
 
     def __str__(self):
         return str(self.produto)
@@ -68,7 +77,10 @@ class ItemVendido(models.Model):
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=255)
-    telefone = models.CharField(max_length=20)
+    contato = models.CharField(max_length=20)
+    rua = models.CharField(max_length=200)
+    numero = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
