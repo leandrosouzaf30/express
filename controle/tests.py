9bc1b29386c63007acef0c4fb4c712be1a165f9a
Leandro Souza <leandrosouzaf30@gmail.com>
2018-10-24 22:31:04 -0300

    Criado teste para criação de produto

--- controle/tests.py
+++ controle/tests.py
@@ -1,14 +1,44 @@
 from django.test import TestCase
 
-from controle.models import Fornecedor
+from controle.models import Fornecedor, Cliente, Categoria, Produto
 
-class FornecedorTestCase(TestCase):
+class ProdutoTestCase(TestCase):
     def setUp(self):
-        self.fornecedor01 = Fornecedor.objects.create(
-                                                        nome='Shopping Mucuripe', 
-                                                        endereco='Av. Principal',
-                                                        cidade='Fortaleza',
-                                                        uf='CE',
-                                                        telefone='20202020',
-                                                        )
+        fornecedor01 = Fornecedor.objects.create(
+            nome='Shopping Mucuripe', 
+            endereco='Av. Principal',
+            cidade='Fortaleza',
+            uf='CE',
+            telefone='20202020',
+        )
+        categoria01 = Categoria.objects.create(
+            nome = 'Blusa Feminina',
+            descricao = 'Blusa Descrição'
+        )
+    
+        produto01 = Produto.objects.create(
+            descricao = 'Descrição teste Produto',
+            preco = 39.0,
+            unidades_compradas = 5,
+            fornecedor = fornecedor01,
+            categoria = categoria01
+        )
+        
+
+# class ClienteTestCase(TestCase):
+#     def setUp(self):
+#         self.cliente01 = Cliente.objects.create(
+#         nome = 'Leandro',
+#         contato = '3014-2855',
+#         rua = '11',
+#         numero = '440',
+#         bairro = 'Novo Maracanau',
+#         )
+# class CategoriaTestCase(TestCase):
+#     def setUp(self):
+#         self.categoria01 = Categoria.objects.create(
+#             nome = 'Blusa Feminina',
+#             descricao = 'Blusa Descrição'
+#         )
+
 
